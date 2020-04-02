b238960545d16678264529b3125de88df8348523
Rsja12 <asocarras10@gmail.com>
2020-04-02 18:25:53 -0400

    imports axios and finish setting up form

--- src/components/auth/Registration.js
+++ src/components/auth/Registration.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export class Registration extends Component {
 
@@ -11,12 +12,14 @@ export class Registration extends Component {
 
     handleSubmit = (e) => {
         console.log('submitted')
+        axios.post("https://localhost:3001/registrations")
         e.preventDefault()
     }
 
     handleChange = (e) => {
         this.setState({
-            email: e.target.value 
+            // uses name attr in input tags to handle change of correct input field
+            [e.target.name]: e.target.value 
         })
     }
 
@@ -30,8 +33,26 @@ export class Registration extends Component {
                     placeholder="Email" 
                     value={this.state.email} 
                     onChange={this.handleChange} 
-                    require 
+                    required 
                     />
+                    <input 
+                    type="password" 
+                    name="password" 
+                    placeholder="Password" 
+                    value={this.state.password} 
+                    onChange={this.handleChange} 
+                    required 
+                    />
+                    <input 
+                    type="password" 
+                    name="password_confirmation" 
+                    placeholder="Password Confirmation"  
+                    value={this.state.password_confirmation} 
+                    onChange={this.handleChange} 
+                    required 
+                    />
+
+                    <button type="submit">Register</button>
                 </form>
             </div>
         )
