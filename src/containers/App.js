aaab5e874a1de99242eda69285b48eb9a5e574fe
Rsja12 <asocarras10@gmail.com>
2020-04-06 15:38:08 -0400

    Remove NavLink and Redirect

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, NavLink, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import axios from 'axios'
 
 import Home from '../components/Home'
